0b759d6b1fbbf1964a9042025686f3a125f64981
Keith Zhuo <haozhuo99@gmail.com>
2024-09-19 00:42:51 -0700

    add python3 solution for TennisTraining

---
+++ problems/TennisTraining/solutions/python3/kzhuo.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+# should we be consistent and use space as delimiter?
+days = [int(day) for day in input().split(",")]
+costs = [int(cost) for cost in input().split(",")]
+
+seven, thirty = deque(), deque()
+total = 0
+
+for day in days:
+    while seven and seven[0][0] <= day-7:
+        seven.popleft()
+    if not seven:
+        seven.append((day, total + costs[1]))
+
+    while thirty and thirty[0][0] <= day-30:
+        thirty.popleft()
+    if not thirty:
+        thirty.append((day, total + costs[2]))
+
+    total = min(total+costs[0], seven[-1][1], thirty[-1][1])
+
+print(total)
